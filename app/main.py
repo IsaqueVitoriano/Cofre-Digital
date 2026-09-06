e0dd37bda3ec7687ece42a98ef242bb13b35f7c7
Isaque Vitoriano <isaquevitoriano7@gmail.com>
2026-09-05 21:03:43 -0300

    corrige indentação no arquivo main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
 from app.core.logging_config import logger
+
 app = FastAPI(
     title="Cofre Digital - Arquivos de Segurança da Informação",
     description="API para gerenciamento, proteção e auditoria de logs e evidências de cibersegurança",
